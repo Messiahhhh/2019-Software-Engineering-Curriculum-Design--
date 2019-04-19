e1acce6211ec834d9492ba47de45882a9d818f18
Messiahhhh <13340077150@163.com>
2019-04-20 00:04:26 +0800

    添加了选课系统中的两张表

--- EMS/courseSelection/models.py
+++ EMS/courseSelection/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db import models
 
 # Create your models here.
+
+from backstage.models import Teacher, Student,\
+    College, MajorPlan, AdmClass, ClassRoom
+from scoreManagement.models import Course,MajorCourses,Teaching
+
+class courseSelected(models.Model):
+    sno = models.ForeignKey(to=Student,on_delete=models.CASCADE)
+    course = models.ForeignKey(to=MajorCourses,on_delete=models.CASCADE)
+    tno = models.ForeignKey(to=Teacher,on_delete=models.CASCADE)
+    time = models.CharField(max_length=20)
+    classroom = models.CharField(max_length=20)
+    score = models.FloatField()
+
+class TeachersCourse(models.Model):
+    tno = models.ForeignKey(to=Teacher,on_delete=models.CASCADE)
+    course = models.ForeignKey(to=MajorCourses,on_delete=models.CASCADE)
+    stu_num = models.IntegerField()
+
+
+
+
+
