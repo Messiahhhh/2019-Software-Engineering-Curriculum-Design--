41734bb1868d4c4c44949f220b58e465e69ec7f0
Messiahhhh <13340077150@163.com>
2019-04-21 18:41:34 +0800

    fixed

--- EMS/graduationManagement/views.py
+++ EMS/graduationManagement/views.py
@@ -4,10 +4,6 @@ from django.http import HttpResponse
 
 def welcome(request):
     return render(request, 'graduationManagement/welcome.html')
-<<<<<<< HEAD
-=======
-
 
 def func(request):
     return HttpResponse("func")
->>>>>>> b12887070dca45507f23fcc57d4d880354fe094e
